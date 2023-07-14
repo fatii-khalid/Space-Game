86f0f4a4314f36ce6c81e2bb43a46c422cc28f7a
Fatima <fatimak2816@gmail.com>
2023-07-15 02:13:01 +0500

    obj.cs

---
+++ obj.cs
@@ -0,0 +1,18 @@
+sing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
